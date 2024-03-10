5f03294e43c068cc747e8e293ab3bda868029535
Steg1118 <purple.oreo7516@gmail.com>
2024-03-10 13:18:59 -0700

    crop NPK subtraction fucniton

--- Assets/Scripts/Crop.cs
+++ Assets/Scripts/Crop.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Crop : MonoBehaviour
 {
+    public float minusN_Nitrogen;
+    public float minusP_Phosphorus;
+    public float minusK_Potassium;
+    Tile tilescript;
     // Start is called before the first frame update
     void Start()
     {
+        tilescript = FindObjectOfType<Tile>();
         // switch(gameObject.tag)
         // {
         //     case("Corn"):
@@ -32,5 +37,11 @@ public class Crop : MonoBehaviour
     void Update()
     {
         
+    }
+    public void removeNPKValues()
+    {
+        tilescript.N_NitrogenValue -= minusN_Nitrogen;
+        tilescript.K_PotassiumValue -= minusK_Potassium;
+        tilescript.P_PhosphorusValue -= minusP_Phosphorus;
     }
 }
